a5d81759adbb9f0bf25244a7886e15ddf1615001
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-23 18:21:40 +0100

    get grid

--- js/script.js
+++ js/script.js
@@ -1,35 +1,24 @@
-console.log('JS OK!')
+/* 
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+BONUS:
+1- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+2- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+*/
 
-const send = () => {
-    btnSend.innerText = 'Annulla';
+//* FUNZIONI DA UTILIZZARE
 
-    const grid = document.getElementById('grid')
-    grid.innerText = '';
-}
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const btnSend = document.getElementById('btn-send');
-btnSend.addEventListener('click', send); {
-    // Dichiaro quante siano le bombe
-    const bombs = 16;
-    // Raccogliamo il livello della tendina
-    const option = document.getElementById('difficulty-choice');
-    // Calcolare in base al livello scelto quante griglie far comparire
-    let cells;
-    let row;
 
-    switch (option) {
-        case 'medium':
-            cells = 89;
-            break;
-        case 'hard':
-            cells = 49;
-            break;
-        default:
-            cells = 100;
-    }
-
-    row = Math.sqrt(cells);
-    // Inserisco una variabile per contare i punti
-    const maxPoint = cells - 
-
-}
+// RECUPERO LA GRIGLIA
+const select = document.getElementById("choices");
+const grid = document.getElementById("grid");
+const button = document.getElementById("start");
